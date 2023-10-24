94b5a5d9b90f205eb5d5bbe14560a0634327fb25
lugwiri <lugwirie@gmail.com>
2023-10-24 04:25:43 +0300

    Major changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # bonsai scammer lookup-express-starter
 
-A simple express app with AI that enables users to look up any phone number/email address/website/name of online dealers/sellers/companies/websites and see if they are scams. the AI searches the internet for reported/mentioned cases based on specific keywords and organizes the info. this is compared with verified contacts/addresses/websites of scams.
+A simple express app with AI that enables users to look up any phone number/email address/website/name of online dealers/sellers/companies/websites and see if they are scams. the AI looks for specific keywords and organizes the info. this is compared with verified contacts/addresses/websites of scams.
--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,62 @@
 var express = require('express');
-//var db = require('../db');
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
-
-/*function fetchBonsai(req, res, next) {
-	db.all('SELECT * FROM bonsai WHERE owner_id = ?', [req.user.id], (err, rows) => {
-		if (err) {
-			return next(err);
-		}
-
-		var bonsai = rows.map((row) => {
-			return {
-				id: row.id,
-				url: '/' + row.id,
-			};
-		});
-		res.locals.bonsai = bonsai;
-		next();
-	});
-}*/
-
 function scrapeWeb(query) {
-    try {
-        // Perform web scraping here
-        const base_url = "https://www.google.com/search?q=";
-        query = `${query}"scam" OR "fraud" OR "scammer" OR "scammers" OR "scamalert" OR "con" OR "caution" OR "mwizi" OR "thief" OR "thieves" site:facebook.com, twitter.com`;
-        const url = `${base_url}${query}`;
-        const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        };
+  try {
+      // Perform web scraping here
+      const base_url = "https://www.google.com/search?q=";
+      query = `${query}"scam" OR "fraud" OR "scammer" OR "scammers" OR "scamalert" OR "con" OR "caution" OR "mwizi" OR "thief" OR "thieves" site:facebook.com, twitter.com`;
+      const url = `${base_url}${query}`;
+      const headers = {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      };
 
-        return axios.get(url, { headers })
-            .then((response) => {
-                // Extract the latest results related to the query.
-                const $ = cheerio.load(response.data);
-                const results = [];
-                $('div.tF2Cxc').each((i, element) => {
-                    const title = $(element).find('h3').text();
-                    const link = $(element).find('a').attr('href');
-                    const date = $(element).find('div.f').text().replace(' · ', '').replace(' mins ago', ' minutes ago').replace(' hours ago', '1 hour ago');
-                    const formatted_date = new Date(date).toISOString().slice(0, 10);
-                    results.push({ title, link, date: formatted_date });
-                    console.log(`${i + 1}. ${title}`);
-                    console.log(`   Link: ${link}\n`);
-                    console.log(`   Date: ${formatted_date}\n`);
-                });
+      return axios.get(url, { headers })
+          .then((response) => {
+              // Extract the latest results related to the query.
+              const $ = cheerio.load(response.data);
+              const results = [];
+              $('div.tF2Cxc').each((i, element) => {
+                  const title = $(element).find('h3').text();
+                  const link = $(element).find('a').attr('href');
+                  const date = $(element).find('div.f').text().replace(' · ', '').replace(' mins ago', ' minutes ago').replace(' hours ago', '1 hour ago');
+                  const formatted_date = new Date(date).toISOString().slice(0, 10);
+                  results.push({ title, link, date: formatted_date });
+                  console.log(`${i + 1}. ${title}`);
+                  console.log(`   Link: ${link}\n`);
+                  console.log(`   Date: ${formatted_date}\n`);
+              });
 
-                return results;
-            })
-            .catch((error) => {
-                throw new Error(`An error occurred while making the request: ${error}`);
-            });
-    } catch (error) {
-        throw error;
-    }
+              if (results.length === 0) {
+                throw new Error(`There have been no reported cases of scam/con regarding ${query}. Please note that you still need to practice caution, stay alert, avoid too good to be true deals, and avoid being desperate or giving in to the urgency created by scammers. A scammer usually uses tricks such as very very low prices for quality products, creates urgency such as "it's the only item remaining, or impersonates real dealers. Regardless of your situation, insist on face to face deals or use registered escrow services. Regarding escrow, you must also check to confirm their authenticity" `);
+              }
+
+              return results;
+          })
+          .catch((error) => {
+              throw new Error(`An error occurred while making the request: ${error}`);
+          });
+  } catch (error) {
+      throw error;
+  }
 }
+
 
 var router = express.Router();
 
 /* GET home page. */
-router.get(
-	'/',
-	(req, res, next) => {
-		if (!req.user) {
-			return res.render('home');
-		}
-		next();
-	},
-	
-		scrapeWeb,
-	(query) => {
-		res.locals.filter = null;
-		res.render('index', { user: req.user });
-	},
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Lookup!' });
+  next();
+},
+
+  scrapeWeb,
+(query) => {
+  //res.locals.filter = null;
+  res.render('index', { title: 'Lookup!' });
+},
 );
 
 module.exports = router;
