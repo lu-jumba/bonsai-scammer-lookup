6918ce2b281f12e3155d0c430e586add2cad8aca
lugwiri <lugwirie@gmail.com>
2023-10-26 04:42:09 +0300

    bingo

--- routes/index.js
+++ routes/index.js
@@ -73,7 +73,7 @@ async function scrapeWeb(query) {
               });*/
 
               if (results.length === 0) {
-                console.log (`There have been no reported cases of scam/conning regarding ${query}. Please note that you still need to practice caution, stay alert, avoid too good to be true deals, and avoid being desperate or giving in to the urgency created by scammers. A scammer usually uses tricks such as very very low prices for quality products, creates urgency such as "it's the only item remaining, or impersonates real dealers. Regardless of your situation, insist on face to face deals." `);
+                throw new Error (`There have been no reported cases of scam/conning regarding ${query}. Please note that you still need to practice caution, stay alert, avoid too good to be true deals, and avoid being desperate or giving in to the urgency created by scammers. A scammer usually uses tricks such as very very low prices for quality products, creates urgency such as "it's the only item remaining, or impersonates real dealers. Regardless of your situation, insist on face to face deals." `);
               }
 
               return results;
